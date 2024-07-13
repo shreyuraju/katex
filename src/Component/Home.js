201f4180cc1f89ddb2638cc9b3adb4d30bde9132
shreyuraju <shreyu.raju@gmail.com>
2024-07-13 10:38:17 +0530

    added Katex RENDER directly to index.html

--- src/Component/Home.js
+++ src/Component/Home.js
@@ -1,34 +1,37 @@
-import React, { useState } from 'react';
-import { renderMath } from '../utils/Katex';
+import React, { useState, useEffect } from 'react';
+// import { renderMath } from '../utils/Katex';
 
 const Home = () => {
-    
-    const [inlineExpression, setInlineExpression] = useState('\\int_0^\\infty x^2 dx');
-    const [blockExpression, setBlockExpression] = useState('\\int_0^\\infty x^2 dx');
- 
-    const inlineMath = renderMath(inlineExpression);
-    const blockMath = renderMath(blockExpression, { displayMode: true });
-    const [expression, setExpression] = useState(
-        'The sum of the infinite series 1 + \\left(1+\\frac{1}{2}\\right)\\left(\\frac{1}{3}\\right)+\\left(1+\\frac{1}{2}+\\frac{1}{2^2}\\right)\\left(\\frac{1}{3^2}\\right)+ \\ldots \\infty is'
-      );
-    
-      const handleChange = (e) => {
-        // Replace single backslashes with double backslashes
-        // const value = e.target.value.replace(/\\/g, '\\\\');
-        // setExpression(value);
-        setExpression(e.target.value);
-      };
+
+
+  const [expression, setExpression] = useState(
+    ''
+  );
+  useEffect(() => {
+    if (window.renderMathInElement) {
+      window.renderMathInElement(document.body);
+    }
+  }, [expression]);
+
+  const handleChange = (e) => {
+    // Replace single backslashes with double backslashes
+    // const value = e.target.value.replace(/\\/g, '\\\\');
+    // setExpression(value);
+    const value = e.target.value;
+    setExpression(value);
+  };
+
   return (
     <div>
-    <h1>KaTeX in React</h1>
-    <p>
+      {/* <h1>KaTeX in React</h1> */}
+      {/* <p>
       Here is an inline math equation: <span dangerouslySetInnerHTML={{ __html: inlineMath }} />
     </p>
     <p>
       Here is a block math equation:
       <div dangerouslySetInnerHTML={{ __html: blockMath }} />
-    </p>
-    <div>
+    </p> */}
+      {/* <div>
       <label>
         Inline Expression:
         <input
@@ -47,23 +50,25 @@ const Home = () => {
           onChange={(e) => setBlockExpression(e.target.value)}
         />
       </label>
-    </div>
-    <div>
-      <h1>KaTeX in React</h1>
-      <p dangerouslySetInnerHTML={{ __html: renderMath(expression) }} />
+    </div> */}
       <div>
-        <label>
-          Expression:
-          <input
-            type="text"
-            value={expression}
-            onChange={handleChange}
-            style={{ width: '100%' }}
-          />
-        </label>
+        <h1>KaTeX in React</h1>
+        {/* <p dangerouslySetInnerHTML={{ __html: renderMath(expression) }} /> */}
+        <p>{expression}</p>
+
+        <div>
+          <label>
+            Expression:
+            <input
+              type="text"
+              value={expression}
+              onChange={handleChange}
+              style={{ width: '100%' }}
+            />
+          </label>
+        </div>
       </div>
     </div>
-  </div>
   )
 }
 
